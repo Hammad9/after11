a37c56fad9cdab9abedf33e3b00a232f69c1823c
Hammad <muhammadhammadali112233@gmail.com>
2022-01-16 21:37:01 +0500

    Here We Write the code of Button and Press Button properties

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,TextInput } from 'react-native';
+import { StyleSheet, Text, View,TextInput ,Button} from 'react-native';
+
 
 
 export default function App() {
+  // use State for name and show name where your name is 
   const [name, setName] = useState('')
+
+  // Intial we set submiteed value false
+  const [submitted, setSubmitted] = useState(false)
+  // Button function on press handler
+  const onPressHandler=()=>{
+      setSubmitted(!submitted);
+  }
   return (
     <View style={styles.body}>
       <Text style={styles.txt}>Please Write Your Name:</Text>
       <TextInput 
         // multiline    for Multiline in a input field
-        secureTextEntry    //for show ... at the place of character
+        // secureTextEntry    //for show ... at the place of character
         style={styles.input}
         placeholder='Please Enter Your Name'
         onChangeText={(val)=>setName(val)}
       />
-      <Text>Your name is: {name}</Text>
+     
+      
+     {
+      submitted? <Text>You are Register as {name}</Text> : null
+    }
+      <Button 
+        
+        title={submitted?'Clear':'Submit' }
+        onPress={onPressHandler}
+      />
     </View>
   );
 }
@@ -35,12 +53,14 @@ const styles = StyleSheet.create({
   },
   txt:{
     fontSize:20,
+    
   },
   input:{
     marginTop:20,
     borderWidth:1,
     paddingLeft:150,
     paddingRight:150,
+    marginBottom:10,
     // height:40,
   }
 });
